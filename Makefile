f2e1c39c88eab296f9b29045219de7e205f14182
Lucas S. Bickel <hairmare@purplehaze.ch>
2011-10-02 00:57:22 +0200

    ebuild for current version of mixxx
    this adds a portage overlay and some settings for getting the
    current version of mixxx to compile. I still need to test the
    whole thing.

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
 
 install: install_ALL
 
-install_ALL: install_puppet
+install_ALL: install_puppet install_repo
 
 install_puppet:
 	cd puppet/modules && \
 	find ./ -type d -exec mkdir -p /usr/share/puppet/modules/{} \; && \
 	find ./ -type f -exec cp    {} /usr/share/puppet/modules/{} \;
+
+install_repo: /usr/local/portage
+
+/usr/local/portage:
+	ln -s `pwd`/portage.repo /usr/local/portage
--- puppet/modules/mixxx/manifests/init.pp
+++ puppet/modules/mixxx/manifests/init.pp
@@ -16,24 +16,35 @@ class mixxx {
   }
   file { '/etc/portage/package.keywords/media-sound/mixxx':
     ensure => file,
-    content => '=media-sound/mixxx-1.6.1 ~x86',
+    content => '=media-sound/mixxx-1.9.0 ~x86',
+  }
+  file { '/etc/portage/package.keywords/media-libs/portmidi':
+    ensure => file,
+    content => '=media-libs/portmidi-217 ~x86',
   }
 
   # mixxx needs qt3support which depends on some qt3 useflags
+  # i'm not actuall 100% sure that this is needed for mixxx-1.9.0
+  # but its still in here since i started building on an old mixxx
+  # version. i'll try to get rid of this in the next iteration.
   file { '/etc/portage/package.use/x11-libs':
     ensure => directory,
   }
   file { '/etc/portage/package.use/x11-libs/qt-gui':
     ensure => file,
-    content => 'x11-libs/qt-gui qt3support'
+    content => 'x11-libs/qt-gui qt3support',
   }
   file { '/etc/portage/package.use/x11-libs/qt-core':
     ensure => file,
-    content => 'x11-libs/qt-core qt3support'
+    content => 'x11-libs/qt-core qt3support',
   }
   file { '/etc/portage/package.use/x11-libs/qt-sql':
     ensure => file,
-    content => 'x11-libs/qt-sql qt3support'
+    content => 'x11-libs/qt-sql qt3support',
+  }
+  file { '/etc/portage/package.use/x11-libs/qt-phonon':
+    ensure => file,
+    content => 'x11-libs/qt-phonon qt2support',
   }
   package { 'x11-libs/qt-qt3support':
     ensure => installed,
